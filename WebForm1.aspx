8d21014bcfc779c234d2af9eef3fcc99fb097fdd
Aqeel Ashraf <33582470+Aq107@users.noreply.github.com>
2020-08-27 08:05:27 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="LBLHeader" runat="server" Text="Welcome to the application"></asp:Label>
+            <br/>
+
+            <asp:Label ID="LBLNAME" runat="server" Text="Full Name : "></asp:Label><br/>
+            <asp:TextBox ID="TXTName" runat="server"></asp:TextBox><br/>
+
+            <asp:Label ID="LBLPassword" runat="server" Text="Password : "></asp:Label><br/>
+            <asp:TextBox ID="TXTPassword" runat="server"></asp:TextBox><br/>
+
+            <asp:Button ID="BtnSave" runat="server" Text="Save" OnClick="BtnSave_Click" /> &nbsp
+            <asp:Button ID="BtnDelete" runat="server" Text="Delete" OnClick="BtnDelete_Click" /> &nbsp
+            <asp:Button ID="BtnGV" runat="server" Text="View Data" OnClick="BtnGV_Click"/>
+
+
+
+            
+            <br />
+            <br />
+            <br />
+
+                <asp:GridView ID="gvdetails" runat="server" AutoGenerateColumns="False">
+                    <Columns>
+                        <asp:BoundField DataField="name" HeaderText="name" />
+                        <asp:BoundField DataField="password" HeaderText="password" />
+                    </Columns>
+            </asp:GridView>
+
+
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            //conn.Open();
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+
+            if (TXTName.Text == "" || TXTPassword.Text == "")
+            {
+                return;
+            }
+
+            string query = "insert into [test].[dbo].[testtable] (name,password) values ('" + TXTName.Text + "','" + TXTPassword.Text + "')";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        protected void BtnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+
+            string delete = "delete from [test].[dbo].[testtable] where name = '"+TXTName.Text+"';";
+            SqlCommand cmddelete = new SqlCommand(delete, conn);
+            cmddelete.ExecuteNonQuery();
+        }
+
+        protected void BtnGV_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            conn.Open();
+
+            SqlDataAdapter sqlda = new SqlDataAdapter("select * from [test].[dbo].[testtable]", conn);
+            DataTable dt1 = new DataTable();
+            sqlda.Fill(dt1);
+
+            gvdetails.DataSource = dt1;
+            gvdetails.DataBind();
+        }
+    }
+}
